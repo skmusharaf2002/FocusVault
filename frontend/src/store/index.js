d8357034d19983309618cb7d558649dcec4c39cd
musharraf10 <skmusharaf00@gmail.com>
2025-06-24 17:10:26 +0530

    Final fix needed

---
+++ frontend/src/store/index.js
@@ -0,0 +1,37 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "@reduxjs/toolkit";
+
+// Import reducers
+import authReducer from "./slices/authSlice";
+import studyReducer from "./slices/studySlice.js";
+import chatReducer from "./slices/chatSlice";
+import uiReducer from "./slices/uiSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth", "study"], // Only persist auth and study data
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  study: studyReducer,
+  chat: chatReducer,
+  ui: uiReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
